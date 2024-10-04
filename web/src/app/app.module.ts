0afc2cad11fe46d32daecb928f89b5783f57ce57
Billy Ardela <billy@propstep.com>
2024-10-03 19:25:46 -0700

    web: filters and sorting working

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -7,18 +7,26 @@ import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { SubdivisionDataDisplayComponent } from './subdivision-data-display/subdivision-data-display.component';
+import { SubdivisionDisplayDetailsComponent } from "./subdivision-display-details/subdivision-display-details.component";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSelectModule } from "@angular/material/select";
+import { SelectFilterComponent } from "./select-filter/select-filter.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SubdivisionDataDisplayComponent
+    SubdivisionDataDisplayComponent,
+    SubdivisionDisplayDetailsComponent,
+    SelectFilterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatPaginatorModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/src/app/subdivision-data-display/subdivision-data-display.component.ts
+++ web/src/app/subdivision-data-display/subdivision-data-display.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { getSubdivisions } from "../services/subdivision";
 
 @Component({
   selector: 'app-subdivision-data-display',
   templateUrl: './subdivision-data-display.component.html',
-  styleUrls: ['./subdivision-data-display.component.css']
+  styleUrls: ['./subdivision-data-display.component.css'],
 })
 export class SubdivisionDataDisplayComponent implements OnInit {
 
+  readonly PAGE_SIZE = 2;
+  readonly FILTER_VALUES = [
+    { label:'Active', value: 'Active' },
+    { label: 'Future', value: 'Future' },
+    { label: 'Builtout', value: 'Builtout' }
+  ];
+  readonly SORT_VALUES = [
+    { label:'name, ASC', value: 'name' },
+    { label:'name, DESC', value: '-name' },
+    { label:'nearMapImageDate, ASC', value: 'nearMapImageDate' },
+    { label:'-nearMapImageDate, DESC', value: '-nearMapImageDate' }
+  ];
+
+  public page: any[] = [];
+  public numberOfResults = this.page.length;
+  public  criteria: Criteria = {
+    filter: '',
+    pageIndex: 0,
+    pageSize: this.PAGE_SIZE,
+    sortField: '',
+  } as Criteria;
+
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    await this.updateResults();
   }
 
+  async updateResults() {
+    const [subdivisions, size] = await getSubdivisions(this.criteria);
+    this.page = subdivisions;
+    this.numberOfResults = size;
+  }
+
+  async handlePagination(event: any) {
+    this.criteria.pageIndex = event.pageIndex;
+    await this.updateResults()
+  }
+
+  async updateFilter(event: any) {
+    this.criteria.filter = event;
+    this.criteria.pageIndex = 0;
+    await this.updateResults();
+  }
+
+  async updateSort(event: any) {
+    this.criteria.sortField = event;
+    this.criteria.pageIndex = 0;
+    await this.updateResults();
+  }
+}
+
+interface Criteria {
+  filter: any,
+  pageIndex: number,
+  pageSize: number,
+  sortField: 'name' | '-name' | 'nearMapImageDate' | '-nearMapImageDate' | ''
 }
